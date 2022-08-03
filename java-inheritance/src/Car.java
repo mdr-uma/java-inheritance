1a75a21b9f0ce14d37d21bad2aa2376fc8cb9653
github username <um_2003_8@hotmail.com>
2022-08-03 16:17:01 -0400

    add a note about extends keyword

--- java-inheritance/src/Car.java
+++ java-inheritance/src/Car.java
@@ -1,4 +1,6 @@
 
+//extends keyword will make a Car class a sub-class of Factory below. extends will inherit all the methods from a super class(Factory) to sub-class(Car)
+
 public class Car extends Factory {
 
 }
