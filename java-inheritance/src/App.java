4bb87e83d63898f3633c907947ba8736dac29516
github username <um_2003_8@hotmail.com>
2022-08-11 17:05:32 -0400

    add a note about private access modifier

--- java-inheritance/src/App.java
+++ java-inheritance/src/App.java
@@ -2,12 +2,13 @@
 
 public class App {
 	public static void main(String[] args) {
-		//access modifiers: public, protected, default(lack of access modifier), private
-		//eg of default method: void design() {
-		//System.out.println("Design the car");
-		//}
-		//public access modifier: can be access to any class anywhere regardless of packages.
-		//protected access modifier: can be access to the class in the same package.
+		/*access modifiers: public, protected, default(lack of access modifier), private
+		eg of default method: void design() {
+		System.out.println("Design the car");
+		}
+		public access modifier: can be access to any class anywhere regardless of packages.
+		protected access modifier: can be access to the class in the same package.
+		private access modifier: can be access only from the same class. can't be access even if its a sub-class. */
 		
 		Factory factory1 = new Factory();
 		
